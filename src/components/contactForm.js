57d1d1a22271ce548d8c43708c6cfff22589467d
Eduardo <eduuhliima97@gmail.com>
2021-01-05 14:08:05 -0300

    concluindo

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -3,37 +3,45 @@ import React from 'react';
 
 export default function ContactUs() {
 
+
     function sendEmail(e) {
         e.preventDefault();
 
     emailjs.sendForm('gmailteste', 'template_lat4php', e.target, 'user_BLg80uyyFXNabuj2m8IGP')
+
         .then((result) => {
-            console.log(result.text);
+            alert("Mensagem enviada com sucesso! 👍");
+           
         }, (error) => {
-            console.log(error.text);
+            alert(error.message)
+            
         });
         e.target.reset()
-    }
 
+ 
+    }
     return(
         <div>
             <div className="container">
+            <h2>Contato</h2>
             <form onSubmit={sendEmail}>
                     <div className="row pt-5 mx-auto">
-                        <div className="col-8 form-group mx-auto">
-                            <input type="text" className="form-control" placeholder="Name" name="name"/>
+                        
+                        <div className="col-lg-8 col-sm-12 form-group mx-auto">
+                            <label>Nome</label>
+                            <input type="text" autoFocus className="form-control" required placeholder="Nome" name="name"/>
                         </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="email" className="form-control" placeholder="Email Address" name="email"/>
+                        <div className="col-lg-8 col-sm-12 form-group pt-1 mx-auto">
+                        <label>Email</label>
+                            <input type="email" className="form-control" required placeholder="Seu email" name="email"/>
                         </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="text" className="form-control" placeholder="Subject" name="subject"/>
+
+                        <div className="col-lg-8 col-sm-12 form-group pt-1 mx-auto">
+                        <label>Mensagem</label>
+                            <textarea className="form-control" id="" cols="30" rows="8" required placeholder="Sua mensagem" name="message"></textarea>
                         </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
-                        </div>
-                        <div className="col-8 pt-3 mx-auto">
-                            <input type="submit" className="btn btn-info" value="Send Message"></input>
+                        <div className="col-lg-8 col-sm-12 pt-3 mx-auto">
+                            <input type="submit" className="btn btn-info" value="Enviar mensagem"></input>
                         </div>
                     </div>
                 </form>
